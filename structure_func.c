365835117a7897fae8d17ac45b8d866ea32d913e
farzana00069 <zannatulfarzana363@gmail.com>
2025-01-29 00:29:40 +0600

    new part added

---
+++ structure_func.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+struct Person{
+    char name[50];
+    int age;
+    float salary;
+};
+void display(struct Person p) {
+    printf("\nName: %s\n", p.name);
+    printf("Age: %d\n", p.age);
+    printf("Salary: %.2f\n", p.salary);
+}
+int main() {
+    struct Person person1, person2;
+    
+    strcpy(person1.name, "Zannatul Farzana"); //string directly initialize kra jbena
+    person1.age=20;
+    person1.salary=2000.500;
+    //function call
+    display(person1);
+    
+    strcpy(person2.name, "Zayed al faisal"); //string directly initialize kra jbena
+    person2.age=27;
+    person2.salary=20000;
+    //function call
+    display(person2);
+
+    return 0;
+}
